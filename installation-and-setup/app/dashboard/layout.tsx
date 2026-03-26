f3e99bccb8bea23ca1ff406377af087f6c0c7b67
itlpnc-dev <it.lpnc@gmail.com>
2026-03-26 10:10:25 +0700

    edit from agent

--- installation-and-setup/app/dashboard/layout.tsx
+++ installation-and-setup/app/dashboard/layout.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 import React from 'react'
 
-const layout = ({children}:{children:React.ReactNode}) => {
+const Layout = ({children}:{children:React.ReactNode}) => {
   return (
     <div className="flex gap-5">
-      <aside className="w-60 h-screen text-slate-800">
-        <div className="bg-blue-800 px-10 p-4 h-30 text-3xl flex items-end text-white rounded">
+      <aside className="w-30 h-screen text-slate-800">
+        <div className="bg-blue-500 px-10 p-4 h-32 text-3xl flex items-end text-white rounded">
           <h1>PJ01</h1>
         </div>
         <nav className="px-5 py-5">
@@ -27,10 +27,10 @@ const layout = ({children}:{children:React.ReactNode}) => {
             </Link>
           </ul>
         </nav>
-        <div>{children}</div>
       </aside>
+      <div>{children}</div>
     </div>
   );
 }
 
-export default layout
+export default Layout
--- installation-and-setup/app/layout.tsx
+++ installation-and-setup/app/layout.tsx
@@ -5,16 +5,16 @@ import React from "react";
 export default function RootLayout({children}:{children: React.ReactNode}){
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-      </head>
+      <meta
+        name="viewport"
+        charSet="UTF-8"
+        content="width=device-width, initial-scale=1.0"
+      />
+      <title>Document</title>
       <body>
         <div className="min-h-screen">{children}</div>
         <footer className="bg-black p-4 text-white">
-          <ul className="flex justify-center items-center gap-5">
-          </ul>
+          <ul className="flex justify-center items-center gap-5"></ul>
         </footer>
       </body>
     </html>
